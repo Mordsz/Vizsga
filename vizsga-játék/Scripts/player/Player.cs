fda319b884f5344e4c0bf0d962b6be157015001b
MateHavasi <havasi.mate@simonyiszki.org>
2025-08-23 12:38:27 +0200

    Animáció
    Ha mozogni kezd a karakter, akkor elkezd lejátszani egy mozgás animációt.

--- vizsga-játék/Scripts/player/Player.cs
+++ vizsga-játék/Scripts/player/Player.cs
@@ -4,7 +4,6 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 80.0f;
-
 	public override void _PhysicsProcess(double delta)
 	{
 		// Get the input direction and handle the movement/deceleration.
@@ -12,5 +11,54 @@ public partial class Player : CharacterBody2D
 		Vector2 direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
 		Velocity = direction * Speed;
 		MoveAndSlide();
+
+		// Ha csak függőleges irányba mozog a karakter
+		if (direction != Vector2.Zero && Input.IsActionPressed("move_left") == false && Input.IsActionPressed("move_right") == false)
+		{
+			// Fel és le mozgás animáció
+
+			// Ha felmegy
+			if (Input.IsActionPressed("move_up"))
+			{
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation = "Up";
+				
+			}
+			// Ha lemegy
+			else if (Input.IsActionPressed("move_down"))
+			{
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation = "Down";
+				
+			}
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play();
+		}
+		// Ha csak vízszintes irányba mozog a karakter
+		else if (direction != Vector2.Zero && Input.IsActionPressed("move_up") == false && Input.IsActionPressed("move_down") == false)
+		{
+			// Jobb és bal mozgás animáció
+
+			// Ha balra megy
+			if (Input.IsActionPressed("move_left"))
+			{
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation = "Right";
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = true;
+			}
+			// Ha jobbra megy
+			else if (Input.IsActionPressed("move_right"))
+			{
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").FlipH = false;
+				GetNode<AnimatedSprite2D>("AnimatedSprite2D").Animation = "Right";
+			}
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play();
+		}
+		// Ha megáll a karakter
+		else if (direction == Vector2.Zero)
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Stop();
+		}
+		else
+		{
+			GetNode<AnimatedSprite2D>("AnimatedSprite2D").Play();
+		}
+
 	}
 }
